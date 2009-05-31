bbfe4c082454ae4da21b0069ee597f38f6c78728
LCID Fire <lcid-fire@gmx.net>
2009-05-31 22:21:29 +0200

    wcscasecmp not available by devkitppc - implement it ourselves

--- src/homeworld.c
+++ src/homeworld.c
@@ -7,3 +7,23 @@
  *==========================================================================*/
 
 /* ... */
+//temporary implemenation for missing devkitppc stuff
+#ifdef GEKKO
+#include <wctype.h>
+int wcscasecmp(const wint_t *s1, const wint_t *s2)
+{
+    while(*s1 && *s2)
+    {
+	const wint_t wc1 = towupper(*s1);
+	const wint_t wc2 = towupper(*s2);
+
+        if(wc1 != wc2)
+		return wc1 - wc2;
+
+	s1++;
+	s2++;
+    }
+
+    return towupper(*s1) - towupper(*s2);
+}
+#endif
